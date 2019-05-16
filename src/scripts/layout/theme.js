2412c4d676b09edbd545c3b38c25708633cf1558
github username <samson.yuwono@gmail.com>
2019-05-16 10:09:59 -0400

    set up flickity anmd styled page header

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,8 +1,8 @@
-import '../../styles/theme.scss';
-import '../../styles/theme.scss.liquid';
+import "../../styles/theme.scss";
+import "../../styles/theme.scss.liquid";
 
-import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
-import {cookiesEnabled} from '@shopify/theme-cart';
+import { focusHash, bindInPageLinks } from "@shopify/theme-a11y";
+import { cookiesEnabled } from "@shopify/theme-cart";
 
 // Common a11y fixes
 focusHash();
@@ -11,7 +11,15 @@ bindInPageLinks();
 // Apply a specific class to the html element for browser support of cookies.
 if (cookiesEnabled()) {
   document.documentElement.className = document.documentElement.className.replace(
-    'supports-no-cookies',
-    'supports-cookies',
+    "supports-no-cookies",
+    "supports-cookies"
   );
 }
+
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  cellAlign: "right",
+  groupCells: 4,
+  contain: true,
+  wrapAround: true
+});
